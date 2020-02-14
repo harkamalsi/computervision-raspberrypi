32d3a5aee59311faeecdd02020c682f3538e81e9
chrisvibe <vibechris@gmail.com>
2020-02-15 00:22:07 +0100

    addded venv for picam_wrapper and tweeked docker

--- docker/setup_docker.sh
+++ docker/setup_docker.sh
@@ -2,10 +2,11 @@
 
 build()
 {
+    sudo docker image rm -f vm_image
     sudo docker build -t vm_image .
 }
 
 run()
 {
-   sudo docker run -it --name vm_container -v $PWD/../:/home vm_image
+   sudo docker run -it --rm --name vm_container -v $PWD/../:/home vm_image
 }
