305a82266dd87b3b26d7dc38ad07e2e01b7a4f1f
harkamalsi <harkamal_guraya@hotmail.com>
2020-02-22 13:37:28 +0100

    Finished get_picture. Main.py is almost done, need oled_wrapper.

--- src/main.py
+++ src/main.py
@@ -1,1 +1,11 @@
-from picam_wrapper import take_picture
+from picam_wrapper import take_picture, get_picture
+
+
+def run():
+    take_picture.take_picture()
+    print('picture taken!')
+    picture = get_picture.get_picture()
+
+
+if __name__ == '__main__':
+    run()
